73ac9ef3538d9cd4b0ca75d27c61e8230934c32c
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-09-14 10:33:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# client_web_url
+
+`client_web_url` A flutter widget for Webview rendering.
+
+
+## Features
+
+ - enabling the use of web content within apps
+
+
+## Getting started
+
+To use this package, add client_web_url as a dependency in your pubspec.yaml file.
+
+## Usage
+
+Minimal example:
+
+```dart
+   WebViewPlugin(url: url,),
+```
+
---
+++ lib/client_web_url.dart
@@ -0,0 +1,20 @@
+library client_web_url;
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPlugin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Hardcode the URL here
+    final String _url = 'https://gkyc.gwcindia.in/client'; // Replace with your desired URL
+
+    // Initialize the WebViewController
+    final WebViewController _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(_url));
+
+    return Scaffold(
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
